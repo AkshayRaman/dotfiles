f2ac92fd617160401e0d19d22024fd5902c7102f
Akshay <2210019+AkshayRaman@users.noreply.github.com>
2017-11-13 10:59:45 -0800

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+echo "START"
+curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.gitconfig > ~/.gitconfig
+curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.vimrc > ~/.vimrc
+curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.screenrc > ~/.screenrc
+echo "DONE"
