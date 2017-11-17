2ad1825fd220e56acf49915e531d12265d94ff42
Akshay <2210019+AkshayRaman@users.noreply.github.com>
2017-11-17 10:01:22 -0800

    Fetch Piperita terminal

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ echo "START"
 curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.gitconfig > ~/.gitconfig
 curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.vimrc > ~/.vimrc
 curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.screenrc > ~/.screenrc
+curl https://raw.githubusercontent.com/jacobtomlinson/terminal-piperita/master/schemes/Terminal/Piperita.terminal > ~/Piperita.terminal
 echo "DONE"
