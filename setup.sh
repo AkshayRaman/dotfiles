a7c52e8b0bcf16fc4e66b1dea6662caa9c475cbf
Akshay <2210019+AkshayRaman@users.noreply.github.com>
2017-11-17 10:32:56 -0800

    Add bash_profile

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ echo "START"
 curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.gitconfig > ~/.gitconfig
 curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.vimrc > ~/.vimrc
 curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.screenrc > ~/.screenrc
+curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.bash_profile > ~/.bash_profile
 curl https://raw.githubusercontent.com/jacobtomlinson/terminal-piperita/master/schemes/Terminal/Piperita.terminal > ~/Piperita.terminal
 echo "DONE"
