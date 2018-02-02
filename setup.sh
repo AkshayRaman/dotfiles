ded4de729f33ba61203ce2f1ce0812a7c6423754
Akshay <2210019+AkshayRaman@users.noreply.github.com>
2018-02-02 15:19:58 -0800

    Add bashrc

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.gitconfig > 
 curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.vimrc > ~/.vimrc
 curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.screenrc > ~/.screenrc
 curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.bash_profile > ~/.bash_profile
-curl https://raw.githubusercontent.com/jacobtomlinson/terminal-piperita/master/schemes/Terminal/Piperita.terminal > ~/Piperita.terminal
+curl https://raw.githubusercontent.com/AkshayRaman/dotfiles/master/.bashrc > ~/.bashrc
+#curl https://raw.githubusercontent.com/jacobtomlinson/terminal-piperita/master/schemes/Terminal/Piperita.terminal > ~/Piperita.terminal
 echo "DONE"
